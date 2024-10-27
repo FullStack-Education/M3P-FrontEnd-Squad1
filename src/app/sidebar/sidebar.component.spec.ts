7a1edb58e4f62ad42c72283ef6b8b8fbac376bda
barbaracalderon <bcalderoni.ti@gmail.com>
2024-10-27 15:05:27 -0300

    test(sidebar): teste de instanciação do sidebar

--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SidebarComponent } from './sidebar.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
 
+
+  const mockActivatedRoute = {
+    params: of({}),
+  };
+
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate'),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SidebarComponent]
+      imports: [SidebarComponent, CommonModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter },
+      ],
     })
     .compileComponents();
     
