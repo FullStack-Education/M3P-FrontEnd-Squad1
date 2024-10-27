0a8f1143a84d24b5bb6c9b0bb4c0c3db3defeebe
LuisPedroCTrindade <luispedrotrindade77@gmail.com>
2024-10-27 18:15:20 -0300

    feature(home-aluno): ordenar avaliações por data

--- src/app/home-aluno/home-aluno.component.ts
+++ src/app/home-aluno/home-aluno.component.ts
@@ -23,9 +23,15 @@ export class HomeAlunoComponent implements OnInit {
       { nome: 'Prova de Matemática', materia: 'Matemática', data: '2024-08-15' },
       { nome: 'Redação', materia: 'Português', data: '2024-08-20' },
       { nome: 'Exame de Física', materia: 'Física', data: '2024-08-25' },
+      { nome: 'Redação II', materia: 'Português', data: '2024-10-20' },
+      { nome: 'Trabalho de Química', materia: 'Química', data: '2024-05-22' }
     ];
 
     this.materias = ['Matemática', 'Português', 'Física'];
+
+    this.avaliacoes = this.avaliacoes
+    .sort((a, b) => new Date(b.data).getTime() - new Date(a.data).getTime())
+    .slice(0, 3);
   }
 
   verMaisAvaliacao(avaliacao: any) {
