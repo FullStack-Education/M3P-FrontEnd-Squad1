5c64dd69ebb1e5d339bc19ec59a2e3b46cd21669
David Alves Dutra <dadutra@hotmail.com>
2024-10-30 09:33:29 -0300

    Login integrado. Tratado erro de login. HttpClient importado.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Inject, inject} from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import {LoginService} from "../services/login.service";
 
 @Component({
   selector: 'app-login',
@@ -11,39 +12,62 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private loginService: LoginService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(3)]]
     });
   }
 
+  token = {
+    token: ''
+  };
+
+  cursos: any;
+
   onSubmit() {
     const { email, password } = this.loginForm.value;
+    this.loginService.login(email, password).subscribe(response =>  this.setToken(response));
 
-    const usersMock = [
-      { email: 'admin@scholargate.com', password: 'admin123', role: 'ADMINISTRADOR', name: 'Marina Oliveira' },
-      { email: 'docente@scholargate.com', password: 'docente123', role: 'DOCENTE', name: 'Otávio Queiroz' },
-      { email: 'aluno@scholargate.com', password: 'aluno123', role: 'ALUNO', name: 'Pedro Torres' }
-    ];
+    // const usersMock = [
+    //   { email: 'admin@scholargate.com', password: 'admin123', role: 'ADMINISTRADOR', name: 'Marina Oliveira' },
+    //   { email: 'docente@scholargate.com', password: 'docente123', role: 'DOCENTE', name: 'Otávio Queiroz' },
+    //   { email: 'aluno@scholargate.com', password: 'aluno123', role: 'ALUNO', name: 'Pedro Torres' }
+    // ];
 
-    const user = usersMock.find(u => u.email === email && u.password === password);
+    // const user = usersMock.find(u => u.email === email && u.password === password);
 
-    if (user) {
-      localStorage.setItem('currentUser', JSON.stringify(user));
-      this.router.navigate(['/home']);
-    } else {
-      alert('Email ou senha incorretos. Tente novamente.');
-    }
+    // if (user) {
+    //   localStorage.setItem('currentUser', JSON.stringify(user));
+    //   this.router.navigate(['/home']);
+    // } else {
+    //   alert('Email ou senha incorretos. Tente novamente.');
+    // }
   }
+
+  setToken(response: any) {
+    this.token = { ...response };
+    
+    sessionStorage.setItem('token', this.token.token)
+
+    console.log(this.token.token);
+  }
+
+  setCursos(response: any) {
+    this.cursos = { ...response };
+
+    console.log(this.cursos);
+  }
+
 
   onRegister() {
     alert('Funcionalidade de cadastro em desenvolvimento.');
   }
 
   onForgotPassword() {
-    alert('Funcionalidade de recuperação de senha em desenvolvimento.');
+    this.loginService.getCursos().subscribe(response => this.setCursos(response))
   }
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http:HttpClient) { }
+
+
+  login(login: string, senha: string) {
+
+    let url = 'http://localhost:8080/login';
+
+    const body = {
+      login: `${login}`,
+      senha: `${senha}`
+    }
+
+    return this.http.post(url, body).pipe(catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMsg = 'Usuário com login ou senha errados.'
+
+    if (error.error instanceof ErrorEvent) {
+      errorMsg = `Error: ${error.error.message}`;
+    } else {
+      errorMsg = `Error código: ${error.status}\nMessage: 'Usuário com login ou senha errados.`
+    }
+
+    alert(errorMsg);
+    return throwError(errorMsg);
+  }
+
+
+  
+
+  getCursos() {
+
+    let url = 'http://localhost:8080/cursos';
+    let token = localStorage.getItem('token');
+
+    console.log('GET!!!')
+    console.log(token)
+
+    let headers = new HttpHeaders({
+      'Authorization' : `${token}`
+    })
+
+    return this.http.get(url, { headers });
+  }
+}
